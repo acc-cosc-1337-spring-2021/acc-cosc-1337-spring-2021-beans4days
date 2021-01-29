6908093a105a14bc6dec247b7bb6ff9eb2014336
beans4days <tsuyao.lin@g.austincc.edu>
2021-01-29 02:51:18 +0000

    Main.cpp Test Upload from Repl.it

--- src/classwork/01_assign/main.cpp
+++ src/classwork/01_assign/main.cpp
@@ -1,11 +1,16 @@
 //Write the include statement for types.h here
-
+#include <iostream>
+#include "types.h"
 //Write include for capturing input from keyboard and displaying output to screen
 
-
+//testing
+using namespace std;
 
 int main() 
 {
+  cout<<"Hello World!\n";
+
+  cin.get();
 
 	return 0;
 }
