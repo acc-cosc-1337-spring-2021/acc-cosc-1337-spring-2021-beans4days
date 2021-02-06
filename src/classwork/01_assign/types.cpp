5b48530d99c0eb50688dc06790ee9f921931748a
beans4days <tsuyao.lin@g.austincc.edu>
2021-02-07 00:19:31 +0800

    HW1

--- src/classwork/01_assign/types.cpp
+++ src/classwork/01_assign/types.cpp
@@ -7,7 +7,19 @@ int sum_numbers(int num1, int num2)
 	return num1 + num2;
 }
 
+// write function code
+// in the function code block - create an int variable named num2, assign it the value 5, and multiply num1 and num2, and return their product.
+int multiply_numbers(int num1) {
 
+int num2 = 5;
+
+num1 * num2;
+
+return num1 * num2;
+
+}
+
+//int num2;
 
 
 
--- src/classwork/01_assign/types.h
+++ src/classwork/01_assign/types.h
@@ -1,5 +1,7 @@
 //DON'T ERASE THIS 
 int sum_numbers(int num1, int num2);
 
+// create an int value-function prototype named multiply_numbers with one int parameter named num1.
 
+int multiply_numbers (int num1);
 
