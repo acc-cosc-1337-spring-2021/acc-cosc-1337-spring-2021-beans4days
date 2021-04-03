bcd8b34e72e84aa443919c05192706e2b43ef74b
beans4days <tsuyao.lin@g.austincc.edu>
2021-04-03 09:24:36 +0800

    HW 6

--- src/homework/tic_tac_toe/tic_tac_toe.h
+++ src/homework/tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,31 @@
 //h
+//tic_tac_toe.h
+//1
+#include<string>
+#include<vector>
+using std::string;
+using std::vector;
+
+class TicTacToe {
+public:
+    //bool game_over;
+    bool game_over();
+    // bool game_over(){return check_board_full;}
+    void start_game(string first_player);
+    void mark_board(int position);
+    string get_player()const{return player;}
+    void display_board()const;
+
+private:
+    void set_next_player();
+    bool check_board_full();
+    void clear_board();
+
+    string player;
+    //vector of string pegs
+    // vector <string> pegs{};
+    // vector <string> pegs={};
+    // vector <string> pegs(9, " ");
+    // vector <string> pegs{9, " "};
+    vector<string> pegs{9, " "};
+    
--- test/homework_test/tic_tac_toe_test/tic_tac_toe_test.cpp
+++ test/homework_test/tic_tac_toe_test/tic_tac_toe_test.cpp
@@ -1,7 +1,19 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "tic_tac_toe.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Verify Unit Test Game Ends Full Board") {
+	TicTacToe Game;
+	Game.start_game("X");
+	for (int i = 1; i < 9; i++)
+	{
+		Game.mark_board(i);
+		REQUIRE(Game.game_over() == false);
+	}
+	Game.mark_board(9);
+	REQUIRE(Game.game_over() == true);
+}
