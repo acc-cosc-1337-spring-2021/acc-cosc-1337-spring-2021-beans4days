081100627096112cba4d95d55b09280a0d53bbda
beans4days <tsuyao.lin@g.austincc.edu>
2021-04-18 04:57:29 +0800

    Old Unbugged Main

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -12,45 +12,73 @@ using std::cin;
 int main() 
 {
 	// new
-	string winner;
-	char option ='Y';
+	TicTacToe Game;
 
-	string question ="X";
+	string winner;
+	string option = "Y";
+	string question;
 	int position;
-	
-do {
-    TicTacToe Game;
-	cout <<"\n" <<"Welcome to TicTacToe" <<"\n\n\n";
-	do {
-	    // Prompt the user for the first player.
-	    cout <<"Enter x or as o?\n";
-	    cin >> question;
-	    cout <<"\n";
-	} while (question != "X" && question != "x" && question != "o" && question != "O");
-	Game.start_game(question);
-	cout <<"\n";
-	Game.display_board();
-	cout<<"\n";
-	
-	do
-	{	
-		cout <<Game.get_player()<<"'s turn." <<"\n";
-		cout <<"Enter position from 1 to 9." <<"\n";
-		cout <<"Number picked: ";
-		cin >> position;
-		cout << "\n";
-		Game.mark_board(position);
-		Game.display_board();
-		cout << "\n";
-	}while(Game.game_over() == false);
-	
-	cout <<"Game Over... " <<"\n\n\n";
-	cout <<"AND THE WINNER IS " << Game.get_winner() <<" !!!" <<"\n\n\n";
-	cout <<"Would you like to play another round ?" <<"\n\n";
-	cout <<"Enter the following (y/n): ";
-	cin >> option;
-	cout <<"\n";
-} while (option == 'Y' || option == 'y');
+
+
+	while(option == "Y" || option == "y")
+	{
+		cout << "Please enter as player X or O ? (CAPITAL LETTERS)\n";
+		cin >> question;
+		cout <<"\n" << "You have enetered: ";
+		cout << question <<"\n";
+
+		// User Input Validation for Player
+		while(!(question == "X" || question == "O")) //|| question!= "x" || question != "o")
+		{
+			cout << "Incorrect Character - Please select X or O only. (CAPITAL LETTERS)\n";
+			cin >> question;
+			cout <<"\n" << "You have enetered: ";
+			cout << question <<"\n";
+		}
+
+		Game.start_game(question);
+
+		while(Game.game_over() == false)
+		{
+			cout <<Game.get_player()<<"'s turn." <<"\n";
+			cout <<"Enter position from 1 to 9." <<"\n";
+			cout <<"Number picked: ";
+			cin >> position;
+			// User Input Validation for Position
+			while(position < 1 || position > 9)
+			{
+				cout << "Invalid position selected, please re-enter position between 1-9.\n";
+				cout << "Number picked: ";
+				cin >> position;
+			}
+
+			cout << "\n";
+			Game.mark_board(position);
+			Game.display_board();
+			cout << "\n";
+		}
+	if(Game.game_over() == true)
+	{
+		cout << "GAME OVER!\n";
+		if(Game.get_winner() == "X" || Game.get_winner() =="O")
+		{
+			cout << "Winner is " <<Game.get_winner() <<"!" <<"\n";
+		}
+		else if (Game.get_winner() == "C")
+		{
+			cout << "It's a DRAW/TIE!" <<"\n";
+			cout << "Winner is " <<Game.get_winner() <<"!" <<"\n";
+		}
+		else
+		{
+			//Nil
+		}
+		// Play again?
+		cout <<"Would you like to play another round? (Y/N): ";
+		cin >> option;
+	}
+	}
 
 	return 0;
+
 }
